05114f2b08a7390e98180fe9d43633b8458e3271
SaangelR10 <75223784+SaangelR10@users.noreply.github.com>
2023-11-21 17:49:38 -0500

    cambios producto y buscador

--- tienda_electronica/app_tienda_electronica/views.py
+++ tienda_electronica/app_tienda_electronica/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render, redirect
 from .forms import ClienteSignUpForm, CustomAuthenticationForm  # Asegúrate de que solo estas clases estén importadas
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login as auth_login
-from .models import Categoria, Producto
+from .models import Producto
 
-# Create your views here.
+
+from django.db.models import Q
+
 def home(request):
-    categorias = Categoria.objects.all()
-    productos = Producto.objects.all()
-    return render(request, 'home.html', {'categorias': categorias, 'productos': productos})
+    productos = Producto.objects.all()  # Obtener todos los productos
+    termino_busqueda = request.GET.get('q')
+
+    if termino_busqueda:
+        # Filtrar productos por nombre o descripción que contengan el término de búsqueda
+        productos = Producto.objects.filter(Q(nombre__icontains=termino_busqueda))
+    else:
+        # Si no se realiza una búsqueda, mostrar todos los productos
+        productos = Producto.objects.all()
+
+    return render(request, 'home.html', {'productos': productos, 'termino_busqueda': termino_busqueda})
+
 
 def register(request):
     if request.method == 'POST':
